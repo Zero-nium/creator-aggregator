2f183855faa58db489519bfc1f64bbce1ce3860b
Sean Dudley <seand713@googlemail.com>
2026-05-14 15:08:00 +0800

    Minor CORS fix

--- backend/main.py
+++ backend/main.py
@@ -68,7 +68,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://creator-aggregator.vercel.app", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
